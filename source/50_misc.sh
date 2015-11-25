5d7e36c0a2b6e8babc3aa655a0e4cee6408de5c2
Callum Loh <cloh@squiz.net>
2015-11-25 13:05:00 +1100

    Updated less defaults.

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -8,7 +8,7 @@ shopt -s checkwinsize
 export GREP_OPTIONS='--color=auto'
 
 # Prevent less from clearing the screen while still showing colors.
-export LESS=-XR
+export LESS=-asrRix8
 
 # Set the terminal's title bar.
 function titlebar() {
