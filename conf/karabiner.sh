413df31f964ec358530123acf067fd37b20170b5
Callum Loh <cloh@squiz.net>
2015-11-27 01:35:16 +1100

    Adding karabiner as a bin rather then the XML.

---
+++ conf/karabiner.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set repeat.initial_wait 416
+/bin/echo -n .
+$cli set private.f192f19_escape 1
+/bin/echo -n .
+/bin/echo
