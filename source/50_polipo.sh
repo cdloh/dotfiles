d57c4a2ca82a31c7023b0396cf9914e5884f3e2d
Callum Loh <EMAIL>
2015-12-08 22:23:24 +1100

    Added purge and cache size abilities to polipo function.

--- source/50_polipo.sh
+++ source/50_polipo.sh
@@ -1,5 +1,5 @@
 function polipo() {
-	local usage="Usage: $0 [-h|--help] [start|stop|status]"
+	local usage="Usage: $0 [-h|--help] [start|stop|status|purge|size]"
 
 	(($#)) || {
 		echo "$usage" >&2
@@ -9,8 +9,11 @@ function polipo() {
 	case "$1" in
 		start) launchctl load -F ~/Library/LaunchAgents/homebrew.mxcl.polipo.plist;;
 		stop) launchctl unload -F ~/Library/LaunchAgents/homebrew.mxcl.polipo.plist;;
+		restart) launchctl unload -F ~/Library/LaunchAgents/homebrew.mxcl.polipo.plist && launchctl load -F ~/Library/LaunchAgents/homebrew.mxcl.polipo.plist;;
 		status) launchctl list | grep polipo;;
 		-h|--help) echo "$usage";;
+		purge) kill -USR1 `pid /usr/local/opt/polipo/bin/polipo` && sleep 1 && /usr/local/bin/polipo -x && kill -USR2 `pid /usr/local/opt/polipo/bin/polipo`;;
+		size) du -sh /usr/local/var/cache/polipo/;;
 		*) echo "Unknown command: $1"; echo "$usage" >&2; return 1;;
 	esac
 }
