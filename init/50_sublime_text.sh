8f46d831f8449cac1f99648f71356f19e71e1bcd
Callum Loh <EMAIL>
2016-08-27 19:09:12 +1000

    fix making the directories.

--- init/50_sublime_text.sh
+++ init/50_sublime_text.sh
@@ -3,11 +3,11 @@ is_osx || return 1
 
 #Create the Installed Packages dir
 e_header "Creating the Installled packages dir"
-mkdir -p "~/Library/Application Support/Sublime Text 3/Installed Packages"
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
 
 #Create the Packages dir for linking the user data
 e_header "Creating the Packages dir for linking user data"
-mkdir -p "~/Library/Application Support/Sublime Text 3/Packages"
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages"
 
 e_header "Linking the user directory"
 ln -sf "$DOTFILES/conf/sublime" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
