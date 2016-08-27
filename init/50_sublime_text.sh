87782413b01a095a9c401117f7d13355b9cdcac5
Callum Loh <EMAIL>
2016-08-27 18:23:58 +1000

    realised i've done this dance before. actually fixed sublime text.

--- init/50_sublime_text.sh
+++ init/50_sublime_text.sh
@@ -10,9 +10,9 @@ e_header "Creating the Packages dir for linking user data"
 mkdir -p "~/Library/Application Support/Sublime Text 3/Packages"
 
 e_header "Linking the user directory"
-ln -sf "$DOTFILES/conf/sublime" "~/Library/Application Support/Sublime Text 3/Packages/User"
+ln -sf "$DOTFILES/conf/sublime" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
 #Install package manager so all of our packages are installed on startup
 #Sublime Text will still need a restart on next boot
 e_header "Installing Package Control"
-curl -o "~/Library/Application Support/Sublime Text 3/Installed Packages/Package Control.sublime-package" https://packagecontrol.io/Package%20Control.sublime-package
+curl -o ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package "https://packagecontrol.io/Package%20Control.sublime-package"
