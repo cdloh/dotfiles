2ac6d472591d47015f15b8a4c908546208b2007b
Callum Loh <EMAIL>
2016-08-27 19:10:42 +1000

    fix

--- init/50_sublime_text.sh
+++ init/50_sublime_text.sh
@@ -7,7 +7,7 @@ mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
 
 #Create the Packages dir for linking the user data
 e_header "Creating the Packages dir for linking user data"
-mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages"
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 
 e_header "Linking the user directory"
 ln -sf "$DOTFILES/conf/sublime" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
